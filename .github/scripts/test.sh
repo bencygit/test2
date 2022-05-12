44a41b7e8e6a0c8a950e95cfcbe1cfa107fc3f33
bencygit <85782865+bencygit@users.noreply.github.com>
2022-05-12 19:49:02 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "Test Shell Script !!"
-echo "Test Shell Script !!" | mail -s "a subject" bencyjacob07@gmail.com
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"
+echo "Test Shell Script !!"

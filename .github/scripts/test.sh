f0af2a1fca29d6be8c326c79bcb11cb005695268
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-11 16:39:29 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
-echo "Test Shell Script !!"
+echo "Hello $1"
+echo "Current time is $2"
+
+if [[ $3 == 1 ]]; then
+  echo "Execute Function-1"
+elif [[ $3 == 2 ]]; then
+  echo "Execute Function-2"
+else
+  echo "Execute Function-3"
+fi

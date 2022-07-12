f883230b5adc7015431b5f3de33e4cbbcf633258
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-12 16:00:34 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -4,10 +4,10 @@ echo "INTERFACE TO BE DEPLOYED:: $1"
 echo "PACKAGE TO BE DEPLOYED:: $4"
 echo "Current time is $2"
 
-if [ $1 == 'TEST_INTERFACE1' ]
+if [[ $1 == 'TEST_INTERFACE1' ]]
 then
   echo "DEPLOY TEST_INTERFACE1"
-elif [ $1 == 'TEST_INTERFACE2' ]
+elif [[ $1 == 'TEST_INTERFACE2' ]]
 then
   echo "DEPLOY TEST_INTERFACE2"
 else

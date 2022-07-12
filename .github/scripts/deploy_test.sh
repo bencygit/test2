170d40504c60f64c2006b79ac99cafd233c7676a
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-12 17:32:24 +0530

    Update deploy_test.sh

--- .github/scripts/deploy_test.sh
+++ .github/scripts/deploy_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Test Shell Script !!"
-echo "INTERFACE TO BE DEPLOYED:: $1"
-echo "PACKAGE TO BE DEPLOYED:: $2"
-echo "Current time is $3"
+echo "INTERFACE TO BE DEPLOYED :: $1"
+echo "PACKAGE TO BE DEPLOYED :: $2"
+echo "Current time is :: $3"
+echo "Cueent Build Number is :: $4"
 
 if [[ $2 == P ]]; then
   echo "DEPLOY TEST_PACKAGE P"

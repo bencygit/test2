8f370d460b5e563880ecc20c57fa2d3c5d14d6fc
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-12 22:25:02 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -11,3 +11,12 @@ elif [[ $1 == 2 ]]; then
 else
   echo "ENTER VALID INTERFACE DETAILS"
 fi
+
+props="./config/jenkins-dev.properties"	
+env.JAVA_HOME = $java.home
+env.SAG_HOME = $sag.ci.home
+env.SYS_PROPS = $deployer.system.properties 
+
+echo "JAVA HOME IS : "$java.home
+echo "SAG_HOME IS" : "$sag.ci.home
+echo "SYS_PROPS IS": "$$deployer.system.properties

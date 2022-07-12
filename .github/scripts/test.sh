a9f99085d60fc05eb0d054d7ac67ba53390a0a29
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-12 23:54:02 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
-echo "Test Shell Script !!"
-echo "INTERFACE TO BE BUILD:: $1"
-echo "PACKAGE TO BE BUILD:: $2"
-echo "Current time is $3"
-
-if [[ $1 == 1 ]]; then
-  echo "BUILD TEST_INTERFACE1"
-elif [[ $1 == 2 ]]; then
-  echo "BUILD TEST_INTERFACE2"
-else
-  echo "ENTER VALID INTERFACE DETAILS"
-fi
 
 props="./config/jenkins-dev.properties"	
-#env.JAVA_HOME = $java.home
-#env.SAG_HOME = $sag.ci.home
-#env.SYS_PROPS = $deployer.system.properties 
-
-#	env.SAG_HOME = props['sag.home']
-#	env.SAG_CI_HOME = "\"" + props['sag.ci.home'] + "\""
-#	env.SYS_PROPS = props['deployer.system.properties']
 
 if [ -f "$props" ]
 then
@@ -36,7 +17,24 @@ echo "JAVA HOME IS = " ${java_home}
 echo "SAG_HOME IS = " ${sag_ci_home}
 echo "SYS_PROPS IS = " ${deployer_system_properties}
 
+#/// setup ANT variables
+env.JAVA_HOME = ${java_home}
+env.SAG_HOME = ${sag_ci_home}
+env.SYS_PROPS = ${deployer_system_properties} 
 
 else
 echo "$props not found."
+fi
+
+echo "Test Build Shell Script !!"
+echo "INTERFACE TO BE BUILD:: $1"
+echo "PACKAGE TO BE BUILD:: $2"
+echo "Current time is $3"
+
+if [[ $1 == 1 ]]; then
+  echo "BUILD TEST_INTERFACE1"
+elif [[ $1 == 2 ]]; then
+  echo "BUILD TEST_INTERFACE2"
+else
+  echo "ENTER VALID INTERFACE DETAILS"
 fi

f70e9d814829cad1c03e61ea24be880e8eb15ba0
bencygit <85782865+bencygit@users.noreply.github.com>
2022-07-12 17:22:53 +0530

    Update deploy_test.sh

--- .github/scripts/deploy_test.sh
+++ .github/scripts/deploy_test.sh
@@ -4,10 +4,10 @@ echo "INTERFACE TO BE DEPLOYED:: $1"
 echo "PACKAGE TO BE DEPLOYED:: $2"
 echo "Current time is $3"
 
-if [[ $1 == 1 ]]; then
-  echo "DEPLOY TEST_INTERFACE1"
-elif [[ $1 == 2 ]]; then
-  echo "DEPLOY TEST_INTERFACE2"
+if [[ $2 == P ]]; then
+  echo "DEPLOY TEST_PACKAGE P"
+elif [[ $2 == Q ]]; then
+  echo "DEPLOY TEST_PACKAGE Q"
 else
   echo "ENTER VALID INTERFACE DETAILS"
 fi
